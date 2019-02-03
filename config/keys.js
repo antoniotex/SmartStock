a1d4c94f27009560e7eacca344dfbcc2a22c5a3b
antoniotex <acm.skt@gmail.com>
2019-02-03 20:38:02 -0200

    Adiciona conexão com o banco de dados

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://tex:123456a@ds221155.mlab.com:21155/smart-stock'
+}
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-import express from 'express'
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('OK')
-})
+const db = require('./config/keys').mongoURI
+
+app.use(bodyParser.json())
+
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Conectado'))
+  .catch(erro => console.log(erro))
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
